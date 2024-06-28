3d06cdc3dfc51b69237c3cb52a8dd0c84f4be314
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2024-06-28 18:16:20 +0900

    init

---
+++ src/stories/Button.test.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './Button';
+
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  ...(await import('./Button.stories')).Primary,
+};
